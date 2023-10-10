2e4d52a5bc449ab3cde17be344764966d2673a29
littleraj <littlerajsunil@gmail.com>
2023-10-10 10:41:11 +0530

    initial

---
+++ remainder/views.py
@@ -0,0 +1,154 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render,redirect
+from django.urls import reverse_lazy
+from django.views.generic import View,TemplateView,FormView,ListView,DetailView
+from remainder.forms import RegistrationForm,LoginForm,TodoCreateForm,TodoChangeForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from remainder.models import Todos
+from django.utils.decorators import method_decorator
+# Create your views here.
+
+def signin_required(fn):
+    def wrapper(request,*args,**kwargs):
+        if not request.user.is_authenticated:
+            messages.error(request,"invalid session")
+            return redirect("signin")
+        else:
+            return fn(request,*args,**kwargs)
+        
+    return wrapper
+
+
+
+
+
+
+
+
+class SignUpView(View):
+    def get(self,request,*args,**kwargs):
+        form=RegistrationForm()
+        return render(request,"signup.html",{"form":form})
+    def post(self,request,*args,**kwargs):
+        form=RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Registration completed successfully")
+            return redirect("signin")
+        else:
+            messages.error(request,"failed to create account")
+            return render(request,"signup.html",{"form":form})
+        
+
+class SignInView(View):
+    def get(self,request,*args,**kwargs):
+        form=LoginForm()
+        return render(request,"login.html",{"form":form})
+    def post(self,request,*args,**kwargs):
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            uname=form.cleaned_data.get("username")
+            pwd=form.cleaned_data.get("password")
+            usr=authenticate(request,username=uname,password=pwd)
+            if usr:
+                login(request,usr)
+                messages.success(request,"login success")
+                return redirect("add-todo")
+            else:
+                messages.error(request,"invalid credentials")
+                return render(request,"login.html",{"form":form})
+@method_decorator(signin_required,name="dispatch")         
+class IndexView(TemplateView):
+    template_name="index.html"
+
+
+
+
+@method_decorator(signin_required,name="dispatch")  
+class TodoCreateView(FormView):
+    def get(self,request,*args,**kwargs):
+        form=TodoCreateForm()
+        return render(request,"remainder/todo_add.html",{"form":form})
+    
+    def post(self,request,*args,**kwargs):
+        form=TodoCreateForm(request.POST)
+        if form.is_valid():
+            # form.save(user=request.user)
+            Todos.objects.create(**form.cleaned_data,user=request.user)
+            messages.success(request,"todo add successfully")
+            return redirect("add-todo")
+        else:
+             messages.success(request,"fsiled")
+             return render(request,"remainder/todo_add.html",{"form":form})
+        
+
+@method_decorator(signin_required,name="dispatch")  
+class TodoListView(ListView):
+    template_name="remainder/todo_list.html"
+    context_object_name="todos"
+    model=Todos
+    def get_queryset(self):
+        qs=Todos.objects.filter(user=self.request.user)
+        return qs
+
+    # def get(self,request,*args,**kwargs):
+    #     qs=Todos.objects.all()
+    #     return render(request,self.template_name,{"todos":qs})
+@method_decorator(signin_required,name="dispatch")  
+class TodoDetailView(DetailView):
+    template_name="remainder/todo_detail.html"
+    context_object_name="todo"
+    model=Todos
+    # def get(self,request,*args,**kwargs):
+    #     id=kwargs.get("pk")
+    #     qs=Todos.objects.get(id=id)
+    #     return render(request,self.template_name,{"todo":qs})
+
+  
+@method_decorator(signin_required,name="dispatch")  
+class TodoUpdateView(View):
+    template_name="remainder/todo_edit.html"
+    form_class=TodoChangeForm
+    model=Todos
+    success_url=reverse_lazy("list-todo")
+
+    # def get(self,request,*args,**kwargs):
+    #     id=kwargs.get("pk")
+    #     obj=Todos.objects.get(id=id)
+    #     form=TodoChangeForm(instance=obj)
+    #     return render(request,self.template_name,{"form":form})
+
+    # def post(self,request,*args,**kwargs):
+    #     id=kwargs.get("pk")
+    #     obj=Todos.objects.get(id=id)
+    #     form=TodoChangeForm(request.POST,instance=obj)
+    #     if form.is_valid():
+    #          form.save()
+    #            return redirect("list-todo")
+    #     else:
+        
+    #          return render(request,"remainder/todo_add.html",{"form":form})
+
+
+
+
+
+@signin_required
+def remove_todo(request,*args,**kwargs):
+    id=kwargs.get("pk")
+    Todos.objects.filter(id=id).delete()
+    return redirect("list-todo")
+
+
+
+
+
+                
+
+        
+
+
+
+
